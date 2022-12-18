e68ef1e073cb7717cc58c0d9b40e5dd9b3025930
Privat <julienprivat@gmail.com>
2022-12-18 14:45:21 +0100

    link

--- FRONT/fawa/components/events/tableLine.js
+++ FRONT/fawa/components/events/tableLine.js
@@ -6,7 +6,7 @@ const TableLine = ({ line, color, back }) => {
 
     const [hover, setHover] = useState(false)
 
-    const { name, horaire, type, date, prix }  = line
+    const { name, horaire, type, date, prix, link }  = line
     
     const today = Date.now()
     const dateEvent = new Date(date)
@@ -35,11 +35,13 @@ const TableLine = ({ line, color, back }) => {
         borderBottom: `1px solid ${color}`
     }}
     >
-        <div>{format(new Date(date), 'eeee dd MMMM', {awareOfUnicodeTokens: true, locale: fr})}</div>
-        <div>{horaire}</div>
-        <div>{type}</div>
-        <div>{name}</div>
-        <div>{prix}</div>
+        <a href={`${link}`}>
+            <div>{format(new Date(date), 'eeee dd MMMM', {awareOfUnicodeTokens: true, locale: fr})}</div>
+            <div>{horaire}</div>
+            <div>{type}</div>
+            <div>{name}</div>
+            <div>{prix}</div>
+        </a>
     </div>
 }
 export default TableLine
