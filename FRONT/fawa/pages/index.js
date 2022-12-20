39bba059aa42b154be7ac8adde57eb63527eb718
Privat <julienprivat@gmail.com>
2022-12-20 17:26:23 +0100

    ss

--- FRONT/fawa/pages/index.js
+++ FRONT/fawa/pages/index.js
@@ -132,7 +132,7 @@ export default function Home({ data }) {
       <svg width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
         <defs>
             <filter id="instagram">
-                <feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur" />    
+                <feGaussianBlur in="SourceGraphic" stdDeviation="7" result="blur" />    
                 <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 90 -10" result="goo" />
                 <feComposite in="SourceGraphic" in2="goo" operator="atop"/>
             </filter>
